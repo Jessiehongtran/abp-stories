92efa22e91a2c0a79ecf3a20cfb02eefca76d56d
Hong Tran <jessietran@fb.com>
2021-12-08 08:29:58 -0800

    testing route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Story from './views/story';
 import Home from './views/home';
+import Test from './views/test';
 import { stories } from './data/stories';
 import { Route, Routes } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ export default class App extends React.Component<any,any> {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/test" element={<Test />} />
          {stories.map(story => 
           <Route 
             path={story.path}
