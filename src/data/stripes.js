13a5b86528adcda2a456953759f0d1e9d414ceb8
Hong Tran <jessietran@fb.com>
2021-12-09 15:53:52 -0800

    added images into homepage and link to story

---
+++ src/data/stripes.js
@@ -0,0 +1,151 @@
+export const stripes = [
+    {
+        id: 1,
+        type: "stripe",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639080857/Group_394_kjnxus.svg",
+        connected_to: 2,
+        alt: "purple open"
+    },
+    {
+        id: 2,
+        type: "image",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639082192/Boutique_not_color_corrected_4_1_lhb4rb.png",
+        shown: false,
+        alt: "behind purple",
+        header: "Andrea's story",
+        author: "Andrea Cosentino, Owner of Bigodiva",
+        location: "São Paulo, Brazil",
+        link: "/bigodiva"
+    },
+    {
+        id: 3,
+        type: "stripe",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639081803/Group_402_dlhgye.svg",
+        connected_to: 2,
+        alt: "purple close"
+    },
+    {
+        id: 4,
+        type: "stripe",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639081650/Group_400_ydmby1.svg",
+        connected_to: 5,
+        alt: "blue open"
+    },
+    {
+        id: 5,
+        type: "image",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639082250/Bluebird_houses_screenshot_2_28_w1ixuc.png",
+        shown: false,
+        alt: "behind blue",
+        header: "Lindiwe's story",
+        author: "Lindiwe Masango, Owner of Lee Ice Cubes",
+        location: "Pretoria, South Africa",
+        link: ""
+    },
+    {
+        id: 6,
+        type: "stripe",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639082591/Group_403_1_hnkjxz.svg",
+        connected_to: 5,
+        alt: "blue close"
+    },
+    {
+        id: 7,
+        type: "stripe",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639082871/Group_404_tcgasj.svg",
+        connected_to: 8,
+        alt: "yellow open"
+    },
+    {
+        id: 8,
+        type: "image",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639082914/Bluebird_houses_screenshot_2_5_tupkhd.png",
+        shown: false,
+        alt: "behind yellow",
+        header: "Jemhra's story",
+        author: "Jemhra Garcia, Owner of Kape Giting",
+        location: "Manila, Philippines",
+        link: "/kape-giting"
+    },
+    {
+        id: 9,
+        type: "stripe",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639082983/Group_405_wfs2me.svg",
+        connected_to: 8,
+        alt: "yellow close"
+    },
+    {
+        id: 10,
+        type: "stripe",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639083501/Group_406_xj5qgf.svg",
+        connected_to: 11,
+        alt: "green open"
+    },
+    {
+        id: 11,
+        type: "image",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639083609/Bluebird_houses_screenshot_2_29_ty3xpk.png",
+        shown: false,
+        alt: "behind green",
+        header: "Pablo's story",
+        author: "Pablo Terrera, Owner of Maricafe",
+        location: "Buenos Aires, Argentina",
+        link: "/maricafe"
+    },
+    {
+        id: 12,
+        type: "stripe",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639083582/Group_407_iqdl1l.svg",
+        connected_to: 11,
+        alt: "green close"
+    },
+    {
+        id: 13,
+        type: "stripe",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639083672/Group_408_aeb1kc.svg",
+        connected_to: 14,
+        alt: "light green open"
+    },
+    {
+        id: 14,
+        type: "image",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639083772/Bluebird_houses_screenshot_2_7_g0d3ps.png",
+        alt: "behind light green",
+        header: "Frank's story",
+        author: "Frank Mortimer, Owner of Frank's Honey",
+        location: "Ridgewood, NJ, USA",
+        link: "/franks-honey"
+    },
+    {
+        id: 15,
+        type: "stripe",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639083834/Group_409_js4rcs.svg",
+        connected_to: 14,
+        alt: "light green close"
+    },
+    {
+        id: 16,
+        type: "stripe",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639084058/Group_410_hgsxud.svg",
+        connected_to: 17,
+        alt: "light purple open"
+    },
+    {
+        id: 17,
+        type: "image",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639084116/Bluebird_houses_screenshot_2_6_hylgyi.png",
+        shown: false,
+        alt: "behind light purple",
+        header: "Stephanie's story",
+        author: "Stephanie Lin, Owner of Kestan",
+        location: "Irvine, CA, USA",
+        link: "/kestan"
+    },
+    {
+        id: 18,
+        type: "stripe",
+        data: "https://res.cloudinary.com/dchyongyd/image/upload/v1639084164/Group_411_sworvl.svg",
+        connected_to: 17,
+        alt: "light purple close"
+    },
+]
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,24 +1,60 @@
 import React from 'react';
+import { stripes } from '../data/stripes';
+import { Link } from 'react-router-dom'
 
-export default class Home extends React.Component {
+export default class Home extends React.Component<any,any> {
     constructor(props){
         super(props);
         this.state = {
-
+            stripeData: stripes
         }
+        this.handleHoverStripe = this.handleHoverStripe.bind(this)
+    }
+
+    handleHoverStripe(toShowStripeId){
+        console.log('hovering', toShowStripeId)
+        let { stripeData } = this.state;
+        stripeData.forEach(stripe => stripe.id === toShowStripeId ? stripe.shown = true : stripe.shown = false)
+        this.setState({ stripeData: stripeData })
     }
 
     render(){
+
+        const { stripeData } = this.state;
+
         return (
-            <div style={{ textAlign: 'left', backgroundColor: "#4B0550", minHeight: '100vh', padding: '10%' }}>
-                <div style={{ fontWeight: 400, fontSize: '90px', lineHeight: '100%', letterSpacing: '-0.02em', color: '#9B69AA'}}>
-                    Ads &
-                    <br/>Business
-                    <br/>Products
+            <div style={{ display: 'flex',  }}>
+                <div style={{ textAlign: 'left', backgroundColor: "#4B0550", minHeight: '100vh', padding: '10%', boxSizing: 'border-box', margin: '6px 0' }}>
+                    <div style={{ fontWeight: 400, fontSize: '90px', lineHeight: '100%', letterSpacing: '-0.02em', color: '#9B69AA'}}>
+                        Ads &
+                        <br/>Business
+                        <br/>Products
+                    </div>
+                    <div style={{ fontWeight: 400, fontSize: '90px', lineHeight: '94.85%', letterSpacing: '-0.02em', color: '#D7C8E1', margin: '40px 0' }}>Stories</div>
+                    <div style={{ fontSize: '18px', lineHeight: '140.35%', color: '#D7C8E1', maxWidth: '600px' }}>The Ads & Business Products (ABP) organization empowers businesses around the world. We are inspired by the ingenuity and bravery of the business community. Hearing their stories is the very heart of our culture.</div>
+                    <div style={{ fontSize: '14px', lineHeight: '140.35%', color: '#D7C8E1', maxWidth: '600px', margin: '20px 0' }}>Powered by ABP Programs</div>
                 </div>
-                <div style={{ fontWeight: 400, fontSize: '90px', lineHeight: '94.85%', letterSpacing: '-0.02em', color: '#D7C8E1', margin: '40px 0' }}>Stories</div>
-                <div style={{ fontSize: '18px', lineHeight: '140.35%', color: '#D7C8E1', maxWidth: '600px' }}>The Ads & Business Products (ABP) organization empowers businesses around the world. We are inspired by the ingenuity and bravery of the business community. Hearing their stories is the very heart of our culture.</div>
-                <div style={{ fontSize: '14px', lineHeight: '140.35%', color: '#D7C8E1', maxWidth: '600px', margin: '20px 0' }}>Powered by ABP Programs</div>
+                <div style={{ display: 'flex', minHeight: '100vh', alignItems: 'center', margin: '0' }}>
+                    {stripeData.map(stripe => 
+                        stripe.type === "stripe" 
+                        ? <img alt={stripe.alt} src={stripe.data} onMouseOver={() => this.handleHoverStripe(stripe.connected_to)} />
+                        : stripe.shown 
+                            ? <Link to={stripe.link}>
+                                <div style={{ position: "relative", minHeight: "100vh", margin: '0', cursor: 'pointer'}}>
+                                    <img alt={stripe.alt} src={stripe.data} style={{marginTop: '5px'}} /> 
+                                    <div className="info" style={{ position: 'absolute', bottom: '100px', left: '20px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
+                                        <p style={{ fontWeight: 'bold', fontSize: '18px', lineHeight: '15px', letterSpacing: '0.085em', textTransform: 'uppercase', color: '#F0EBF5', marginBottom: '30px'}}>FEATURE</p>
+                                        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px'}}>
+                                            <div style={{ marginRight: '15px', fontSize: '34px', lineHeight: '32px', color: '#F1F4F7' }}>{stripe.header}</div>
+                                            <img src="https://res.cloudinary.com/dchyongyd/image/upload/v1639087269/Group_175_1_hpivlr.svg"/>
+                                        </div>
+                                        <p style={{fontSize: '18px', lineHeight: '140%', color: '#EBF2E6' }}>{stripe.author}</p>
+                                        <p style={{fontSize: '18px', lineHeight: '140%', color: '#EBF2E6', marginTop: '-10px' }}>{stripe.location}</p>
+                                    </div>
+                                </div>
+                              </Link>
+                            : null)}
+                </div>
             </div>
         )
     }
