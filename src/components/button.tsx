29be4067a8e8b7a266b1246337229b632828c7ec
Hong Tran <jessietran@fb.com>
2022-01-18 08:53:55 -0800

    done styling stories page

---
+++ src/components/button.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Button extends React.Component <any,any> {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+
+        const { text, bgColor, textColor, link } = this.props;
+
+        return (
+                <div style={{ display: 'flex', backgroundColor: `${bgColor}`, alignItems: 'center', padding: '0 10%', borderRadius: '40px', cursor: 'pointer' }}>
+                    <Link to={link} style={{textDecoration: 'none'}}>
+                        <div style={{ display: 'flex'}}>
+                            <p style={{ fontFamily: 'Optimistic Display', fontSize: '24px', lineHeight: '31px', color: `${textColor}`, marginTop: '15px' }}>{text}</p>
+                            <img style={{ marginLeft: '10px'}} src="https://res.cloudinary.com/dfulxq7so/image/upload/v1642523604/Group_175_2_culabe.svg" alt="white right arrow"/>
+                        </div>
+                    </Link>
+                </div>
+            
+        )
+    }
+}
+
--- src/views/stories.jsx
+++ src/views/stories.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Collate from '../components/collate';
+import Button from '../components/button';
 
 export default class Stories extends React.Component {
     constructor(props){
@@ -19,15 +20,16 @@ export default class Stories extends React.Component {
     render(){
         return (
             <div style={{ background: '#EBF2E6', minHeight: '100vh', maxWidth: '100vw', display: 'flex', alignItems: 'center' }}>
-                <div className="content" style={{ maxWidth: '600px', boxSizing: 'border-box', padding: '0 8% 6% 8%' }}>
+                <div className="content" style={{ maxWidth: '600px', boxSizing: 'border-box', padding: '0 8% 6% 8%', display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
                     <div className="title" style={{ fontSize: '80px', lineHeight: '115.35%', color: '#006B4E', textAlign: 'left' }}>
                         Stories
                     </div>
                     <div className="text" style={{ display: 'flex', flexDirection: 'column', textAlign: 'left', fontSize: '18px', lineHeight: '140%', color: '#006B4E'}}>
-                        <p>Transitioning to a New Era</p>
+                        <p style={{ fontFamily: 'Optimistic Display', fontSize: '30px', lineHeight: '116.5%' }}>Transitioning to a New Era</p>
                         <p>Trends and innovation that were once thought to be years away have been accelerated by recent disruptions. The future has arrived ahead of schedule and whether they’re just starting out or staying vital, all businesses are adapting accordingly.</p>
                         <p>This transformation has not always been easy or by choice, but the future is bright for businesses that are ushering in the new era.</p>
                     </div>
+                    <Button text={"View Stories"} bgColor={"#289664"} textColor={"#EBF2E6"} link={""} />
                 </div>
                 <div className="images">
                     <Collate images={this.state.images}/>
